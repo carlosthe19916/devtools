6952ef24bbd5ea66ed2aed3b40979c87cb029453
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2026-08-03 17:45:58 +0200

    fix: ui

--- devcontainer/github.com/pulp/pulp-ui/.devcontainer/initializeCommand.sh
+++ devcontainer/github.com/pulp/pulp-ui/.devcontainer/initializeCommand.sh
@@ -4,6 +4,3 @@ set -euo pipefail
 echo "devcontainerID ${1:-}"
 
 mkdir -p "${HOME}/.config/gcloud"
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-mkdir -p "${SCRIPT_DIR}/pulp/settings/certs"
